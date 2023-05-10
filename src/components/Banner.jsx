776f1bf8f4087b134f5dfb5fb12425b1ebd5ac28
Aaron Lee <aaronlee97667@gmail.com>
2023-05-09 22:01:49 -0500

    Updated Banner Background

---
+++ src/components/Banner.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { ArrowRightCircle } from 'react-bootstrap-icons'
+import headerImg from '../assets/img/header-img.svg'
+import header from '../assets/img/headerimg.svg'
+
+
+function Banner() {
+    const [ loopNum, setLoopNum ] = useState(0);
+    const [ isDeleting, setIsDeleting ] = useState(false);
+    const toRotate = [ "Software Engineer", "Full Stack Developer", "Innovative Programmer" ];
+    const [ text, setText ] = useState('');
+    const [ delta, setDelta ] = useState(300 - Math.random() * 100);
+    const period = 500;
+
+    useEffect(() => {
+        let timer = setInterval(() => {
+            time();
+        }, delta)
+
+        return () => { clearInterval(timer) }
+    }, [text])
+
+    const time = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 1.25)
+        }
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return (
+        <section className='banner' id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className='tagline'>Welcome to my Portfolio!</span>
+                        <h1>{`Hi I'm webdecoded `}<span className='wrap'>{text}</span></h1>
+                        <p>About myself</p>
+                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={header} alt="Headder Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export default Banner
